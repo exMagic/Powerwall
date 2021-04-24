3ebd46e388ffa438c51b4004e46cdc0e8a4a04a9
Maciej Anuszkiewicz <maciek.anuszkiewicz@gmail.com>
2021-04-24 16:27:34 +0200

    mdash 1

---
+++ ESP/Try/Sensor-mDash/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+// Minimal Arduino sketch for mdash.net
+//
+// - Install mDash library:
+//   - Select "Sketch" &rarr; "Include Library" &rarr; "Manage Libraries"
+//   - In the search field, type "mDash" and press Enter
+//   - Click on "Install" to install the library
+// - Select "Tools" → "Board" → "ESP32 Dev Module"
+// - Select "Tools" → "Partitioning Scheme" → "Minimal SPIFFS"
+// - Select "Tools" → "Port" → your serial port
+// - Click on "Upload" button to build and flash the firmware
+//
+// See https://mdash.net/docs/ for the full IoT product reference impementation
+
+#define MDASH_APP_NAME "MinimalApp10"
+#include <mDash.h>
+
+#include <WiFi.h>
+
+// #define WIFI_NETWORK "MyWifiNetworkName"
+// #define WIFI_PASSWORD "MyWifiPassword"
+// #define DEVICE_PASSWORD "mDashDeviceToken"
+
+#define WIFI_NETWORK "exesp"
+#define WIFI_PASSWORD "placuszki2"
+#define DEVICE_PASSWORD "onKc6iNij4vhttOoCljA0A"
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+  mDashBegin(DEVICE_PASSWORD);
+}
+
+void loop() {
+  delay(100);
+}
